89cb23bfb0e95305539b54d9d0a193061c44eed9
Raflyderian <111224512+Raflyderian@users.noreply.github.com>
2022-08-14 17:17:09 +0700

    Add files via upload

---
+++ cari-java.bat
@@ -0,0 +1,15 @@
+@Echo OFF
+Home
+Cls
+Title Cari File
+If Exist "*.Java" (
+   Echo Ada file Java pada direktori ini
+   Echo
+   Pause
+
+)Else (
+ Cls
+ Echo Tidak ada file Java pada direktori ini
+ Echo
+ Pause
+)
